914fb5d5bc3d22d64baa27f645307c3cecb54bc6
wpernath <w.pernath@gmx.de>
2021-04-20 10:35:50 +0200

    Message configuration

--- src/main/java/org/wanja/quarkus/GreetingResource.java
+++ src/main/java/org/wanja/quarkus/GreetingResource.java
@@ -5,12 +5,17 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello-resteasy")
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@Path("/hello")
 public class GreetingResource {
+
+    @ConfigProperty(name = "app.greeting", defaultValue = "Hello, Hi and welcome")
+    private String message;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasy";
+        return message;
     }
 }
